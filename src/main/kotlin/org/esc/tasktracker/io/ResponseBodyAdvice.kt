7c60160020277853105ec8c39a14d981876992b3
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-02-11 21:34:31 +0300

    build(api): created rest controller advice for pretty api responses

---
+++ src/main/kotlin/org/esc/tasktracker/io/ResponseBodyAdvice.kt
@@ -0,0 +1,27 @@
+package org.esc.tasktracker.io
+
+import org.springframework.core.MethodParameter
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+@ControllerAdvice
+class ResponseBodyAdvice : ResponseBodyAdvice<Any> {
+
+    override fun supports(
+        returnType: MethodParameter?,
+        converterType: Class<out HttpMessageConverter<*>?>?
+    ) = true
+
+    override fun beforeBodyWrite(
+        body: Any?,
+        returnType: MethodParameter?,
+        selectedContentType: MediaType?,
+        selectedConverterType: Class<out HttpMessageConverter<*>?>?,
+        request: ServerHttpRequest?,
+        response: ServerHttpResponse?
+    ) = body as? BasicSuccessfulResponse<*> ?: BasicSuccessfulResponse(body)
+}
