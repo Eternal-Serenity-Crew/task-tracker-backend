705ed2f5a07208fa5744192357e6b6d1426e35f7
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-02-11 22:04:34 +0300

    feat(users): created users entity

---
+++ src/main/kotlin/org/esc/tasktracker/entities/Users.kt
@@ -0,0 +1,34 @@
+package org.esc.tasktracker.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.Instant
+
+@Entity
+@Table(name = "users")
+data class Users(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false)
+    var name: String,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(nullable = false)
+    var password: String,
+
+    @CreatedDate
+    var createdAt: Instant,
+
+    @LastModifiedDate
+    var updatedAt: Instant,
+)
