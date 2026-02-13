6102ce18435e2d7c18feb6e3034cd10c9a3272d8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-02-14 00:52:05 +0300

    build(core): updated jpa config

--- src/main/kotlin/org/esc/tasktracker/entities/Users.kt
+++ src/main/kotlin/org/esc/tasktracker/entities/Users.kt
@@ -2,16 +2,19 @@ package org.esc.tasktracker.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener::class)
 data class Users(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,8 +30,8 @@ data class Users(
     var password: String,
 
     @CreatedDate
-    var createdAt: Instant,
+    var createdAt: Instant?,
 
     @LastModifiedDate
-    var updatedAt: Instant,
+    var updatedAt: Instant?,
 )
