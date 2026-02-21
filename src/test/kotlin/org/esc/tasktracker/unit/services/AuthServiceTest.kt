8ecf09e2c323b99f884552245af5d21f8bc43f6a
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-02-22 00:46:56 +0300

    test(auth): created user login tests

--- src/test/kotlin/org/esc/tasktracker/unit/services/AuthServiceTest.kt
+++ src/test/kotlin/org/esc/tasktracker/unit/services/AuthServiceTest.kt
@@ -6,9 +6,11 @@ import io.mockk.mockk
 import io.mockk.runs
 import io.mockk.verify
 import org.esc.tasktracker.exceptions.DoubleRecordException
+import org.esc.tasktracker.exceptions.JwtAuthenticationException
 import org.esc.tasktracker.security.JwtUtil
 import org.esc.tasktracker.services.AuthService
 import org.esc.tasktracker.services.UsersService
+import org.esc.tasktracker.unit.generators.createLoginUserDto
 import org.esc.tasktracker.unit.generators.createUser
 import org.esc.tasktracker.unit.generators.createUserCreateDto
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -53,7 +55,6 @@ class AuthServiceTest {
         @Test
         fun `should throw exception when user already exists`() {
             val createUserDto = createUserCreateDto()
-            val user = createUser()
             val uuid = UUID.randomUUID()
 
             every { usersService.create(createUserDto) } throws DoubleRecordException("Пользователь с email уже существует.")
@@ -61,6 +62,72 @@ class AuthServiceTest {
             assertThrows<DoubleRecordException> { service.register(createUserDto, uuid) }
 
             verify { usersService.create(createUserDto) }
+            verify(exactly = 0) { jwtUtil.removeOldRefreshTokenByUUID(uuid) }
+            verify(exactly = 0) { jwtUtil.generateAccessToken(any()) }
+            verify(exactly = 0) { jwtUtil.generateRefreshToken(any()) }
+        }
+    }
+
+    @Nested
+    inner class LoginTests {
+
+        @Test
+        fun `should login user and return tokens`() {
+            val user = createUser()
+            val uuid = UUID.randomUUID()
+            val loginDto = createLoginUserDto(user.email, user.password)
+
+            every { usersService.getByEmail(any(), any()) } returns user
+            every { passwordEncoder.matches(any(), any()) } returns true
+            every { jwtUtil.removeOldRefreshTokenByUUID(uuid) } just runs
+            every { jwtUtil.generateAccessToken(any()) } returns "accessToken"
+            every { jwtUtil.generateRefreshToken(any()) } returns "refreshToken"
+
+            val result = service.login(loginDto, uuid)
+
+            assertEquals("accessToken", result.accessToken)
+            assertEquals("refreshToken", result.refreshToken)
+
+            verify { usersService.getByEmail(user.email, false) }
+            verify { jwtUtil.removeOldRefreshTokenByUUID(uuid) }
+            verify { jwtUtil.generateAccessToken(any()) }
+            verify { jwtUtil.generateRefreshToken(any()) }
+        }
+
+        @Test
+        fun `should throw exception when user's password is invalid`() {
+            val user = createUser()
+            val uuid = UUID.randomUUID()
+            val loginDto = createLoginUserDto(user.email, "wrong_password")
+
+            every { usersService.getByEmail(any(), any()) } returns user
+            every { passwordEncoder.matches(any(), any()) } returns false
+
+            val exception = assertThrows<JwtAuthenticationException> { service.login(loginDto, uuid) }
+
+            assertEquals(exception.message, "Неверные логин или пароль.")
+
+            verify { usersService.getByEmail(user.email, false) }
+            verify { passwordEncoder.matches(loginDto.password, user.password) }
+            verify(exactly = 0) { jwtUtil.removeOldRefreshTokenByUUID(uuid) }
+            verify(exactly = 0) { jwtUtil.generateAccessToken(any()) }
+            verify(exactly = 0) { jwtUtil.generateRefreshToken(any()) }
+        }
+
+        @Test
+        fun `should throw exception when user doesn't exists`() {
+            val user = createUser()
+            val uuid = UUID.randomUUID()
+            val loginDto = createLoginUserDto("wrong@mail.ru", user.password)
+
+            every { usersService.getByEmail(any(), any()) } returns null
+
+            val exception = assertThrows<JwtAuthenticationException> { service.login(loginDto, uuid) }
+
+            assertEquals(exception.message, "Неверные логин или пароль.")
+
+            verify { usersService.getByEmail(loginDto.email, false) }
+            verify(exactly = 0) { passwordEncoder.matches(user.password, user.password) }
             verify(exactly = 0) { jwtUtil.removeOldRefreshTokenByUUID(uuid) }
             verify(exactly = 0) { jwtUtil.generateAccessToken(any()) }
             verify(exactly = 0) { jwtUtil.generateRefreshToken(any()) }
