57313548c7ced94397abbd07200c24c20826fa52
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-02-25 02:58:56 +0300

    docs(config): created KDoc for all extensions classes

--- src/main/kotlin/org/esc/tasktracker/extensions/StringToHttpResponse.kt
+++ src/main/kotlin/org/esc/tasktracker/extensions/StringToHttpResponse.kt
@@ -2,4 +2,20 @@ package org.esc.tasktracker.extensions
 
 import org.esc.tasktracker.io.BasicSuccessfulResponse
 
+/**
+ * Converts a string message into a standardized successful HTTP response.
+ *
+ * Extension function that wraps a string result into a [BasicSuccessfulResponse]
+ * for consistent API response formatting. Useful for returning simple success
+ * messages from controller endpoints.
+ *
+ * @return [BasicSuccessfulResponse] containing this string as the message payload.
+ *
+ * @see BasicSuccessfulResponse
+ * @see org.esc.tasktracker.interfaces.CrudController.deleteById
+ * @see org.esc.tasktracker.interfaces.CrudController.deleteAll
+ *
+ * @author Vladimir Fokin
+ * @since 1.0
+ */
 fun String.toHttpResponse(): BasicSuccessfulResponse<String> = BasicSuccessfulResponse(this)
